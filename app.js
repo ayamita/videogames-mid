1406ee36b06dafd150496512bf6b6a16b6a2877d
Carlos Chacon <cchacon266@gmail.com>
2021-01-29 01:12:07 -0600

    xd

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ app.use('/dashIndex', dashIndex);
 app.use('/dashPendientesAutorizar', dashPendientesAutorizar);
 
 //Hola 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
