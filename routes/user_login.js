dc0589dc2fb302922380f3b3cfc6a49ad29bc43a
Carlos Chacon <cchacon266@gmail.com>
2021-01-29 02:01:13 -0600

    as

---
+++ routes/user_login.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var db=require("../conexion/conexion");
+
+/* GET home page (login.ejs). */
+router.get('/', function(req, res, next) {
+  res.render('user_login');
+});
+
+/* POST home page (login.ejs). */
+router.post('/', function(req, res, next) {
+    const { email, password } = req.body;
+    const datos = {
+        email,
+        password
+    }
+    if (email && password) {
+        db.query('SELECT * FROM users WHERE email = ? AND password = ?', [datos.email, datos.password], function(err,resultados){
+            if(resultados.length > 0){
+                res.redirect('productos');
+            }else{
+                res.send('El correo o contraseña es incorrecto');
+            }
+            res.end();
+            console.log(resultados);
+        });
+        }else{
+        res.send('Favor de ingresar correo y contraseña');
+        res.end();
+    }
+});
+
+module.exports = router;
