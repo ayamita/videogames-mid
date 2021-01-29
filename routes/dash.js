c14da014e3d906fb8d5242ea8207e75d55de6a1d
Angel Yam <amgelyam@gmail.com>
2021-01-28 18:26:45 -0600

    Correcciones ultimas

--- routes/dash.js
+++ routes/dash.js
@@ -5,8 +5,7 @@ var db=require("../conexion/conexion");
 
 /* GET home page (dash_index.ejs). */
 router.get('/', function(req, res, next) {
-
-  db.query("SELECT * FROM users", function(err,resultados){
+  db.query("SELECT * FROM games", function(err,resultados){
     console.log(resultados);
     res.render('dash_index', { title: 'Videogames', Games:resultados });
   });
